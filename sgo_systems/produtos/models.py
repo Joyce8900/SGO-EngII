633b7c7f578b08b7f5fe7a45e93d7028f8e6087e
Joyce8900 <joycesantos8900@gmail.com>
2025-04-09 18:08:05 -0300

    feat(views): adicionado cadastrar_produtos #19

--- sgo_systems/produtos/models.py
+++ sgo_systems/produtos/models.py
@@ -1,13 +1,20 @@
-from django.db import models
+from django.db import models 
 
 class Categorias(models.Model):
   nome = models.CharField(max_length=100, blank=False, null=False, help_text='Este campo é obrigatório')
-
-
-
+  def __str__(self):
+    return self.nome
 class Produtos(models.Model):
   nome = models.CharField(max_length=100, blank=False, null=False, help_text='Este campo é obrigatório')
   preco = models.FloatField(null=False, help_text="Insira o valor do produto")
   quantidade = models.IntegerField(null=False, help_text="Insira a quantidade.")
   categoria = models.ForeignKey('categorias.Categorias', on_delete=models.PROTECT, help_text="Selecione uma categoria.")
+  cor = models.CharField(max_length=30)
+  tamanho = models.CharField(max_length=100)
+  marca = models.CharField(max_length=100)  
+  descricao = models.CharField(max_length=1000)
+  marca = models.CharField(max_length=100)
   
+  def __str__(self):
+    return self.nome
+
--- sgo_systems/produtos/views.py
+++ sgo_systems/produtos/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, request, redirect
+from .models import Produtos
 # Create your views here.
+class cadastrar_produtos(request):
+    if request.method == 'GET':
+      render(request, "cadastrar_produtos.html")
+    else:
+      nome = request.POST.get("nome")
+      preco = request.POST.get("preco")
+      quantidade = request.POST.get("quantidade")
+      cor = request.POST.get("cor")
+      tamanho = request.POST.get("tamanho")
+      marca = request.POST.get("marca")
+      descricao = request.POST.get("descricao")
+
+      produto = Produtos(nome=nome,preco=preco,quantidade=quantidade,cor=cor,tamanho=tamanho,marca=marca,descricao=descricao)
+      produto.save()
+      redirect ("listar_produtos")
+      
