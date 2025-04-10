c3299c36252cab9ae71e567a6c3135496eb77356
Joyce8900 <joycesantos8900@gmail.com>
2025-04-10 18:19:34 -0300

    feat(views) retatorado views cadastraar_produtos

--- sgo_systems/produtos/views.py
+++ sgo_systems/produtos/views.py
@@ -1,19 +1,39 @@
-from django.shortcuts import render, request, redirect
+from django.shortcuts import render, redirect
 from .models import Produtos
-# Create your views here.
-class cadastrar_produtos(request):
-    if request.method == 'GET':
-      render(request, "cadastrar_produtos.html")
-    else:
-      nome = request.POST.get("nome")
-      preco = request.POST.get("preco")
-      quantidade = request.POST.get("quantidade")
-      cor = request.POST.get("cor")
-      tamanho = request.POST.get("tamanho")
-      marca = request.POST.get("marca")
-      descricao = request.POST.get("descricao")
+from .forms import ProdutoForm
 
-      produto = Produtos(nome=nome,preco=preco,quantidade=quantidade,cor=cor,tamanho=tamanho,marca=marca,descricao=descricao)
-      produto.save()
-      redirect ("listar_produtos")
-      
+def cadastrar_produtos(request):
+    if request.method == "POST":
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = ProdutoForm()
+            return render(request, 'cadastrar_produto.html', {'form': form})
+    else:
+        
+        form = ProdutoForm()
+
+    return render(request, "cadastrar_produto.html", {"form": form})
+
+
+# def cadastrar_produtos(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        preco = request.POST.get('preco')
+        quantidade = request.POST.get('quantidade')
+        cor = request.POST.get('cor')
+        tamanho = request.POST.get('tamanho')
+        marca = request.POST.get('marca')
+        descricao = request.POST.get('descricao')
+        
+        produto = Produtos(
+            nome=nome,
+            preco=preco,
+            quantidade=quantidade,
+            cor=cor,
+            tamanho=tamanho,
+            marca=marca,
+            descricao=descricao
+        )
+        produto.save()
+    return render(request, 'cadastrar_produto.html')  
