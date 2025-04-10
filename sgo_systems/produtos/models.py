79071e6f0ea5ef225e29029b02c82de85f0fec8e
Joyce8900 <joycesantos8900@gmail.com>
2025-04-10 18:15:05 -0300

    feat(excluir) models categorias e marcas

--- sgo_systems/produtos/models.py
+++ sgo_systems/produtos/models.py
@@ -1,14 +1,12 @@
 from django.db import models 
 
-class Categorias(models.Model):
-  nome = models.CharField(max_length=100, blank=False, null=False, help_text='Este campo é obrigatório')
-  def __str__(self):
-    return self.nome
+
+  
 class Produtos(models.Model):
   nome = models.CharField(max_length=100, blank=False, null=False, help_text='Este campo é obrigatório')
   preco = models.FloatField(null=False, help_text="Insira o valor do produto")
   quantidade = models.IntegerField(null=False, help_text="Insira a quantidade.")
-  categoria = models.ForeignKey('categorias.Categorias', on_delete=models.PROTECT, help_text="Selecione uma categoria.")
+  # categoria = models.ForeignKey('categorias.Categorias', on_delete=models.PROTECT, help_text="Selecione uma categoria.")
   cor = models.CharField(max_length=30)
   tamanho = models.CharField(max_length=100)
   marca = models.CharField(max_length=100)  
