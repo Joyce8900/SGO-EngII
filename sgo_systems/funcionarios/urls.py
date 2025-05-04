dd5ef3f0d9fa3801a4257f847f88cdfde473a43a
Gabriel Gomes <gabegomes2020@gmail.com>
2025-05-04 20:29:27 -0300

    feat: editar e excluir funcionario #38

--- sgo_systems/funcionarios/urls.py
+++ sgo_systems/funcionarios/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastrar_funcionario', views.cadastrar_funcionario, name="cadastrar_funcionario"),
-    path('listar_funcionario', views.listar_funcionario, name="listar_funcionario"), 
+    path('cadastrar_funcionario/', views.cadastrar_funcionario, name='cadastrar_funcionario'),
+    path('', views.listar_funcionario, name='listar_funcionario'),
+    path('editar/<int:id>/', views.editar_funcionario, name='editar_funcionario'),
+    path('deletar/<int:id>/', views.excluir_funcionario, name='deletar_funcionario'),
 ]
