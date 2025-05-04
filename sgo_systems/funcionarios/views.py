b41a154b0ff7f41e5e8cbbbadbd88df1be71efb7
Gabriel Gomes <gabegomes2020@gmail.com>
2025-05-03 21:20:10 -0300

    feat(create): cadastrar funcionario

--- sgo_systems/funcionarios/views.py
+++ sgo_systems/funcionarios/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Funcionario
 
 # Create your views here.
 def cadastrar_funcionario(request):
-    ...
+    if request.method == 'GET':
+        return render(request, 'cadastrar_funcionario.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        cargo = request.POST.get('cargo')
+        telefone = request.POST.get('telefone')
+
+        funcionario = Funcionario(
+            nome=nome,
+            cargo=cargo,
+            telefone=telefone,
+        )
+
+        funcionario.save()
+
+        return redirect('cadastrar_funcionario')
