f974cc0f13a6433576cdd1dffef573c4260123f8
Joyce8900 <joycesantos8900@gmail.com>
2025-05-06 14:05:23 -0300

    feat(models):adicionado models.

--- sgo_systems/core/settings.py
+++ sgo_systems/core/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'usuarios',
     'modelo',
     'fornecedores',
+    'marca',
+    
 ]
 
 MIDDLEWARE = [
--- sgo_systems/marca/models.py
+++ sgo_systems/marca/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Marca (models.Model):
+  nome = models.CharField(unique=True,max_length=50, blank=False, null=False)
+
+  def __str__(self):
+    return self.nome
