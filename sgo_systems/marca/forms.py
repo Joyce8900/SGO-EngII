8e83ae1edf1bc9b657366c04a835008a8ea64111
Joyce8900 <joycesantos8900@gmail.com>
2025-05-06 14:16:34 -0300

    feat(forms): adiciona forms.

---
+++ sgo_systems/marca/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Marca
+
+class MarcaForm(forms.ModelForm):
+    class Meta:
+        model = Marca
+        fields = ('nome')
+        labels = {
+            'nome': 'Nome da Marca'   
+        }
