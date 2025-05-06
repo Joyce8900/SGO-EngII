72af58d9156dded59ee69b089eb5637cd3d0a96d
Joyce8900 <joycesantos8900@gmail.com>
2025-05-06 18:01:09 -0300

    feat(editar): adiciona funcionalidade editar #46

--- sgo_systems/marca/views.py
+++ sgo_systems/marca/views.py
@@ -24,3 +24,15 @@ def deletar_marca(request, pk):
   marca = Marca.objects.get(pk=pk)
   marca.delete()
   return redirect("listar_marcas")
+
+def editar_marca(request, pk):
+    marca = marca.objects.get(pk=pk)
+    if request.method == "POST":
+        form = MarcaForm(request.POST, instance=marca)
+        if form.is_valid():
+            form.save()
+            return redirect("listar_marcas")
+    else:
+        form = MarcaForm(instance=marca)
+
+    return render(request, "editar_marca.html", {"form": form})
