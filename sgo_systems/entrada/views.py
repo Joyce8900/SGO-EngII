e51ab53e5959992ccdbb20ca733d85ea6c2cc525
Joyce8900 <joycesantos8900@gmail.com>
2025-05-29 14:38:05 -0300

    refact #69

--- sgo_systems/entrada/views.py
+++ sgo_systems/entrada/views.py
@@ -1,63 +1,44 @@
-from django.shortcuts import redirect, render, get_object_or_404
-from .forms import EntradaForm, PesquisaEntradaForm
-from .models import Entrada
-from produtos.models import Produtos
+# from django.shortcuts import redirect, render, get_object_or_404
+# from .forms import EntradaForm, PesquisaEntradaForm
+# from .models import Entrada
+# from produtos.models import Produtos
 from django.db.models import Q
+from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Entrada
+from .forms import EntradaForm
+from produtos.models import Produtos
+from .forms import PesquisaEntradaForm
 
+class CadastrarEntradaView(View):
+    template_name = 'cadastrar_entrada.html'
 
-def cadastrar_entrada(request):
-    if request.method == 'POST':
+    def get(self, request):
+        form = EntradaForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
         form = EntradaForm(request.POST)
         if form.is_valid():
             entrada = form.save(commit=False)
             produto = entrada.produto
-            print("Antes:", produto.quantidade)
             produto.quantidade += entrada.quantidade
             produto.save()
             entrada.save()
-            print("Depois:", produto.quantidade)
             return redirect('listar_entrada')
-        else:
-            print("Erros do formulário:", form.errors) 
-            return render(request, 'cadastrar_entrada.html', {'form': form})
-    else:
-        form = EntradaForm()
-        return render(request, 'cadastrar_entrada.html', {'form': form})
+        return render(request, self.template_name, {'form': form})
 
 
-def listar_entrada(request):
-    form = PesquisaEntradaForm(request.GET)
-    entradas = Entrada.objects.all().order_by('data_entrada')
+class EditarEntradaView(View):
+    template_name = 'editar_entrada.html'
 
-    if form.is_valid():
-        termo = form.cleaned_data.get('termo')
-        funcionario = form.cleaned_data.get('funcionario')
-        if termo:
-            entradas = entradas.filter(
-                Q(produto__nome__icontains=termo) | 
-                Q(fornecedor__nome__icontains=termo) |
-                Q(funcionario__nome__icontains=termo) 
-            )
-        if funcionario:
-            entradas = entradas.filter(funcionario=funcionario)
-    return render(request, "listar_entrada.html", {
-        "entrada": entradas, 
-        "form": form,
-    })
+    def get(self, request, pk):
+        entrada = get_object_or_404(Entrada, pk=pk)
+        form = EntradaForm(instance=entrada)
+        return render(request, self.template_name, {'form': form})
 
-
-def excluir_entrada(request, pk):
-    entrada = get_object_or_404(Entrada, pk=pk)
-    produto = entrada.produto
-    produto.quantidade -= entrada.quantidade
-    produto.save()
-    entrada.delete()
-    return redirect("listar_entrada")
-
-
-def editar_entrada(request, pk):
-    entrada = get_object_or_404(Entrada, pk=pk)
-    if request.method == 'POST':
+    def post(self, request, pk):
+        entrada = get_object_or_404(Entrada, pk=pk)
         form = EntradaForm(request.POST, instance=entrada)
         if form.is_valid():
             nova_entrada = form.save(commit=False)
@@ -67,6 +48,39 @@ def editar_entrada(request, pk):
             produto.save()
             nova_entrada.save()
             return redirect('listar_entrada')
-    else:
-        form = EntradaForm(instance=entrada)
-    return render(request, 'editar_entrada.html', {'form': form})
+        return render(request, self.template_name, {'form': form})
+
+
+class ExcluirEntradaView(View):
+    def post(self, request, pk):
+        entrada = get_object_or_404(Entrada, pk=pk)
+        produto = entrada.produto
+        produto.quantidade -= entrada.quantidade
+        produto.save()
+        entrada.delete()
+        return redirect("listar_entrada")
+
+    
+
+class ListarEntradaView(View):
+    template_name = 'listar_entrada.html'
+
+    def get(self, request):
+        form = PesquisaEntradaForm(request.GET)
+        entradas = Entrada.objects.all().order_by('data_entrada')
+
+        if form.is_valid():
+            termo = form.cleaned_data.get('termo')
+            funcionario = form.cleaned_data.get('funcionario')
+            if termo:
+                entradas = entradas.filter(
+                    Q(produto__nome__icontains=termo) | 
+                    Q(fornecedor__nome__icontains=termo) |
+                    Q(funcionario__nome__icontains=termo) 
+                )
+            if funcionario:
+                entradas = entradas.filter(funcionario=funcionario)
+        return render(request, self.template_name, {
+            "entrada": entradas, 
+            "form": form,
+        })
