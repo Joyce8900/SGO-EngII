e0f25cd93995e79e73dcb199dcce8c8116e2f499
Gustavo-DSC <gustavodouglas646@gmail.com>
2025-06-11 22:13:56 -0300

    feat(home): adiciona tela home do projeto #81

---
+++ sgo_systems/home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def home_view(request):
+    modules = [
+        {'name': 'Fornecedores', 'url': 'fornecedores/'},
+        {'name': 'Produtos', 'url': 'produtos/listar_produtos/'},
+        {'name': 'Modelos', 'url': 'modelo/listar_modelos/'},
+        {'name': 'Marcas', 'url': 'marca/listar_marcas/'},
+        {'name': 'Funcionários', 'url': 'funcionarios/'},
+        {'name': 'Clientes', 'url': 'clientes/'},
+        {'name': 'Entradas', 'url': 'entrada/listar/'},
+    ]
+    
+    return render(request, 'home/home.html', {'modules': modules})
