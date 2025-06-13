4c745a276988b3c3e469207e829768fc0c0ca256
Gustavo-DSC <gustavodouglas646@gmail.com>
2025-06-13 18:24:01 -0300

    fix(security): restringe home_view para aceitar apenas GET #100

--- sgo_systems/home/views.py
+++ sgo_systems/home/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
 
+@require_GET  # Restringe apenas a GET
 def home_view(request):
-    if request.method == 'GET':
-        modules = [
-            {'name': 'Fornecedores', 'url': 'fornecedores/'},
-            {'name': 'Produtos', 'url': 'produtos/listar_produtos/'},
-            {'name': 'Modelos', 'url': 'modelo/listar_modelos/'},
-            {'name': 'Marcas', 'url': 'marca/listar_marcas/'},
-            {'name': 'Funcionários', 'url': 'funcionarios/'},
-            {'name': 'Clientes', 'url': 'clientes/'},
-            {'name': 'Entradas', 'url': 'entrada/listar/'},
-        ]
-        return render(request, 'home/home.html', {'modules': modules})
-    else:
-        return render(request, 'home/home.html', {'modules': []})
+    modules = [
+        {'name': 'Fornecedores', 'url': 'fornecedores/'},
+        {'name': 'Produtos', 'url': 'produtos/listar_produtos/'},
+        {'name': 'Modelos', 'url': 'modelo/listar_modelos/'},
+        {'name': 'Marcas', 'url': 'marca/listar_marcas/'},
+        {'name': 'Funcionários', 'url': 'funcionarios/'},
+        {'name': 'Clientes', 'url': 'clientes/'},
+        {'name': 'Entradas', 'url': 'entrada/listar/'},
+    ]
+    return render(request, 'home/home.html', {'modules': modules})
