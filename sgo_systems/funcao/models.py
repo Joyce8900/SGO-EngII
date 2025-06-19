2f8c53448bc91871e28a8e2286a4ab215797eb9d
Gabriel Gomes <gabegomes2020@gmail.com>
2025-06-19 17:12:31 -0300

    feat: criando models para funcao #106

--- sgo_systems/funcao/models.py
+++ sgo_systems/funcao/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+# Constantes
+TEXTO_CAMPO_OBRIGATORIO = "Campo obrigatório"
+
+
+class Funcao(models.Model):
+    nome = models.CharField(
+        max_length=255,
+        verbose_name="Nome da Função",
+        help_text=TEXTO_CAMPO_OBRIGATORIO,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    salario = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Salário",
+        help_text=TEXTO_CAMPO_OBRIGATORIO,
+        blank=False,
+        null=False,
+    )
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name = "Função"
+        verbose_name_plural = "Funções"
+        ordering = ['nome']
