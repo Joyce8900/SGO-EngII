16a8a0d98ca2057b8710ba8f14448082e71250cd
Gabriel Gomes <gabegomes2020@gmail.com>
2025-06-19 18:43:38 -0300

    feat: cria urls para funcao #106

---
+++ sgo_systems/funcao/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('cadastrar_funcao/', views.CadastrarFuncaoView.as_view(), name='cadastrar_funcao'),
+    path('', views.ListarFuncaoView.as_view(), name='listar_funcoes'),
+    path('editar/<int:id>/', views.EditarFuncaoView.as_view(), name='editar_funcao'),
+    path('deletar/<int:id>/', views.DeletarFuncaoView.as_view(), name='deletar_funcao'),
+]
