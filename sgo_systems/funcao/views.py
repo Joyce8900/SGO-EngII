619f412875f2cf4ff650df0ac298215892a84c4d
Gabriel Gomes <gabegomes2020@gmail.com>
2025-06-19 18:31:46 -0300

    feat: criar view para cadastro de função #106

--- sgo_systems/funcao/views.py
+++ sgo_systems/funcao/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Funcao
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+class CadastrarFuncaoView(View):
+    template_name = 'funcao/cadastrar_funcao.html'
+
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+    
+    def post(self, request, *args, **kwargs):
+        nome = request.POST.get('nome')
+        salario = request.POST.get('salario')
+
+        funcao = Funcao(nome=nome, salario=salario)
+        funcao.save()
+
+        return redirect('listar_funcoes')
