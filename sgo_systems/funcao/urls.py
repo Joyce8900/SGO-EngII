9d3d554ff2b3f7c4b7051382b73c1a8778be5d49
Gabriel Gomes <gabegomes2020@gmail.com>
2025-06-20 19:13:06 -0300

    fix: corrigindo deleção #106

--- sgo_systems/funcao/urls.py
+++ sgo_systems/funcao/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('cadastrar_funcao/', views.CadastrarFuncaoView.as_view(), name='cadastrar_funcao'),
     path('', views.ListarFuncaoView.as_view(), name='listar_funcoes'),
-    path('editar/<int:id>/', views.EditarFuncaoView.as_view(), name='editar_funcao'),
-    path('deletar/<int:id>/', views.DeletarFuncaoView.as_view(), name='deletar_funcao'),
+    path('editar/<int:pk>/', views.EditarFuncaoView.as_view(), name='editar_funcao'),
+    path('deletar/<int:pk>/', views.DeletarFuncaoView.as_view(), name='deletar_funcao'),
 ]
--- sgo_systems/funcao/views.py
+++ sgo_systems/funcao/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.views import View
 from .models import Funcao
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
 class CadastrarFuncaoView(View):
-    template_name = 'funcao/templates/form_funcao.html'
+    template_name = 'funcao/form_funcao.html'
 
 
     def get(self, request, *args, **kwargs):
@@ -22,7 +23,7 @@ class CadastrarFuncaoView(View):
     
 
 class ListarFuncaoView(View):
-    template_name = 'funcao/templates/listar_funcao.html'
+    template_name = 'funcao/listar_funcao.html'
 
     def get(self, request, *args, **kwargs):
         filtro_nome = request.GET.get('filtro_nome', '')
@@ -35,22 +36,22 @@ class ListarFuncaoView(View):
         
 
 class EditarFuncaoView(View):
-    template_name = 'funcao/templates/form_funcao.html'
+    template_name = 'funcao/form_funcao.html'
 
-    def get(self, request, id, *args, **kwargs):
-        funcao = get_object_or_404(Funcao, id=kwargs['id'])
+    def get(self, request, *args, **kwargs):
+        funcao = get_object_or_404(Funcao, id=kwargs['pk'])
         return render(request, self.template_name, {'funcao': funcao})
     
-    def post(self, request, id, *args, **kwargs):
-        funcao = get_object_or_404(Funcao, id=kwargs['id'])
+    def post(self, request, *args, **kwargs):
+        funcao = get_object_or_404(Funcao, id=kwargs['pk'])
         funcao.nome = request.POST.get('nome')
         funcao.salario = request.POST.get('salario')
         funcao.save()
         return redirect('listar_funcoes')
 
 class DeletarFuncaoView(View):
-    def post(self, request, id, *args, **kwargs):
-        funcao = get_object_or_404(Funcao, id=kwargs['id'])
+    def post(self, request, *args, **kwargs):
+        funcao = get_object_or_404(Funcao, id=kwargs['pk'])
         funcao.delete()
         messages.success(request, "Função deletada com sucesso!")
         return redirect('listar_funcoes')
