d9eb1e05adaca2be6f12eb59aaa94246d0366b5e
Joyce8900 <joycesantos8900@gmail.com>
2025-06-24 17:18:08 -0300

    feat(crud): marca

--- sgo_systems/produtos/urls.py
+++ sgo_systems/produtos/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from .views import CadastrarProdutos
 
 urlpatterns = [
-  path('cadastrar_produto/', views.cadastrar_produtos, name='cadastrar_produto'), 
+  path('cadastrar_produto/', CadastrarProdutos.as_view(), name='cadastrar_produto'), 
   path('listar_produtos/', views.listar_produtos, name='listar_produtos'),
   path('editar_produto/<int:pk>', views.editar_produto, name='editar_produto'),
   path('excluir_produto/<int:pk>', views.excluir_produto, name='excluir_produto'),
