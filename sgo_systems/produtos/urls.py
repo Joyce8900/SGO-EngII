ce65d4193c869b24bae83c56d8e330706c732aca
Gustavo-DSC <gustavodouglas646@gmail.com>
2025-07-14 12:55:04 -0300

    fix(produtos/urls): organizar rotas #113

--- sgo_systems/produtos/urls.py
+++ sgo_systems/produtos/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+
+app_name = 'produtos'
+
 urlpatterns = [
   path('cadastrar_produto/', views.cadastrar_produtos, name='cadastrar_produto'), 
-  path('listar_produtos/', views.listar_produtos, name='listar_produtos'),
+  path('', views.listar_produtos, name='listar_produtos'),
   path('editar_produto/<int:pk>', views.editar_produto, name='editar_produto'),
   path('excluir_produto/<int:pk>', views.excluir_produto, name='excluir_produto'),
   
