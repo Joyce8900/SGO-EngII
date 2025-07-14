08cc489dc9141711d3fbbbf551cc59680ba4cd9b
Gustavo-DSC <gustavodouglas646@gmail.com>
2025-07-14 12:55:03 -0300

    fix(modelo/urls): atualizar endpoints #113

--- sgo_systems/modelo/urls.py
+++ sgo_systems/modelo/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 from .views import CadastrarModelo, ListarModelos, EditarModelo, DeletarModelo
 
+
+app_name = 'modelo'
+
+
 urlpatterns = [
-    path('listar_modelos/', ListarModelos.as_view(), name='listar_modelos'),
+    path('', ListarModelos.as_view(), name='listar_modelos'),
     path('cadastrar_modelo/', CadastrarModelo.as_view(), name='cadastrar_modelo'),
     path('editar_modelo/<int:pk>', EditarModelo.as_view(), name='editar_modelo' ),
     path('deletar_modelo/<int:pk>', DeletarModelo.as_view(), name='deletar_modelo' ),
