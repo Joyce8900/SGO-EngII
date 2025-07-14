8a5f77851e10c7b17c2857902438158fbf018193
Gustavo-DSC <gustavodouglas646@gmail.com>
2025-07-14 12:55:01 -0300

    fix(home/views): atualizar view principal #113

--- sgo_systems/home/views.py
+++ sgo_systems/home/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-from django.views.decorators.http import require_GET
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+#from django.views.decorators.http import require_GET
 
-@require_GET  # Restringe apenas a GET
-def home_view(request):
-    modules = [
-        {'name': 'Fornecedores', 'url': 'fornecedores/'},
-        {'name': 'Produtos', 'url': 'produtos/listar_produtos/'},
-        {'name': 'Modelos', 'url': 'modelo/listar_modelos/'},
-        {'name': 'Marcas', 'url': 'marca/listar_marcas/'},
-        {'name': 'Funcionários', 'url': 'funcionarios/'},
-        {'name': 'Clientes', 'url': 'clientes/'},
-        {'name': 'Entradas', 'url': 'entrada/listar/'},
-    ]
-    return render(request, 'home/home.html', {'modules': modules})
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class HomeView(View):
+    def get(self, request):
+        modules = [
+            {'name': 'Fornecedores', 'url': 'fornecedores/'},
+            {'name': 'Produtos', 'url': 'produtos/listar_produtos/'},
+            {'name': 'Modelos', 'url': 'modelo/listar_modelos/'},
+            {'name': 'Marcas', 'url': 'marca/listar_marcas/'},
+            {'name': 'Funcionários', 'url': 'funcionarios/'},
+            {'name': 'Clientes', 'url': 'clientes/'},
+            {'name': 'Entradas', 'url': 'entrada/listar/'},
+        ]
+        return render(request, 'home/home.html', {'modules': modules})
