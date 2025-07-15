556ec3189f0c4dafb686f18b4658c50acacf28e3
Gustavo-DSC <gustavodouglas646@gmail.com>
2025-07-15 14:59:30 -0300

    fix(marca): ajustes no formulário de marca #113

--- sgo_systems/marca/forms.py
+++ sgo_systems/marca/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import Marca
 
 class MarcaForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Adiciona 'form-control' a todos os campos do formulário
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+    
     class Meta:
         model = Marca
         fields = ('nome',)
